a96558897dc86e39311a84b574016b493427f0f3
Vinayak pandia <vinayakpandia@Vinayaks-MacBook-Air-2.local>
2023-02-13 20:50:18 +0530

    added city models and migrations

---
+++ src/models/city.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model, Sequelize
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class City extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  City.init({
+    name:{
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'City',
+  });
+  return City;
+};
